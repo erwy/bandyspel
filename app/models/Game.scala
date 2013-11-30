fe8bf8e81e322afaf07d853a87a35f491b7568dd
Erik Wyke <erik.wyke@gmail.com>
2013-11-30 21:43:09 +0100

    somechanges

--- app/models/Game.scala
+++ app/models/Game.scala
@@ -7,35 +7,39 @@ import play.api.Play.current
 
 case class Game(
                 id: Long,
+                hometeam_id: Long,
+                awayteam_id: Long,
                 hometeam: String,
                 awayteam: String,
                 gamenum: Long,
-                date: Date,
+                date: Option[Date],
                 homegoals: Int,
                 awaygoals: Int)
 
 object Game {
   val game = {
       SqlParser.get[Long]("id") ~
+      SqlParser.getAliased[Long]("hometeam_id") ~
+      SqlParser.getAliased[Long]("awayteam_id") ~
       SqlParser.getAliased[String]("a") ~
       SqlParser.getAliased[String]("b") ~
       SqlParser.get[Long]("matchnumber") ~
-        SqlParser.get[Date]("datetime") ~
+        SqlParser.get[Option[Date]]("datetime") ~
         SqlParser.get[Int]("homegoals") ~
       SqlParser.get[Int]("awaygoals") map {
-      case id ~ ht ~ awayteam ~ gamenum ~ date ~ homegoals ~ awaygoals => Game( id, ht, awayteam,gamenum, date, homegoals, awaygoals)
+      case id ~ ht_id ~ at_id ~ ht ~ awayteam ~ gamenum ~ date ~ homegoals ~ awaygoals => Game( id, ht_id, at_id, ht, awayteam,gamenum, date, homegoals, awaygoals)
     }
   }
 
 
   def all(): List[Game] = DB.withConnection {
     implicit c =>
-      SQL("SELECT t1.name as a, t2.name as b, g.* FROM game g JOIN team t1 ON t1.id = g.hometeam_id JOIN team t2 ON t2.id = g.awayteam_id").as(game *)
+      SQL("SELECT t1.name as a, t2.name as b,t1.id as hometeam_id, t2.id as awayteam_id, g.* FROM game g JOIN team t1 ON t1.id = g.hometeam_id JOIN team t2 ON t2.id = g.awayteam_id").as(game *)
   }
 
   def getForTeam(id: Long): List[Game] = DB.withConnection {
     implicit c =>
-      SQL("SELECT t1.name as a, t2.name as b, g.* FROM game g JOIN team t1 ON t1.id = g.hometeam_id JOIN team t2 ON t2.id = g.awayteam_id WHERE g.hometeam_id = {id} or g.awayteam_id = {id}").on("id"->id).as(game *)
+      SQL("SELECT t1.name as a, t2.name as b,t1.id as hometeam_id, t2.id as awayteam_id, g.* FROM game g JOIN team t1 ON t1.id = g.hometeam_id JOIN team t2 ON t2.id = g.awayteam_id WHERE g.hometeam_id = {id} or g.awayteam_id = {id}").on("id"->id).as(game *)
   }
 
 
